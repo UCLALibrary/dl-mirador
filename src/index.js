583ce9702754446440b056390beeb507f45d541e
pghorpade <pghorpade@library.ucla.edu>
2021-09-21 13:53:09 -0700

    Check for no manifest url

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import Mirador from 'mirador/dist/es/src/index';
 
 let params = new URL(document.location).searchParams;
 let  encodedArk;
-let manifestURL
+let manifestURL = "";
 let initializedManifest = params.get('manifest');
-let splitURL = initializedManifest.split("ark:");
-if(splitURL.length > 1){
-  encodedArk = "ark:" + splitURL[1].substring(0,splitURL[1].lastIndexOf("/")).replaceAll("/","%2F");
-  console.log("Ark ENCODED "+encodedArk);
-  manifestURL = splitURL[0] + encodedArk + splitURL[1].substring(splitURL[1].lastIndexOf("/"));
-  console.log("URL ENCODED "+manifestURL);
-} else {
-  manifestURL = initializedManifest;
-  console.log("URL ENCODED "+manifestURL);
+if(initializedManifest) {
+  let splitURL = initializedManifest.split("ark:");
+  if(splitURL.length > 1){
+    encodedArk = "ark:" + splitURL[1].substring(0,splitURL[1].lastIndexOf("/")).replaceAll("/","%2F");
+    console.log("Ark ENCODED "+encodedArk);
+    manifestURL = splitURL[0] + encodedArk + splitURL[1].substring(splitURL[1].lastIndexOf("/"));
+    console.log("URL ENCODED "+manifestURL);
+  } else {
+    manifestURL = initializedManifest;
+    console.log("URL ENCODED "+manifestURL);
+  }
 }
+
 
 
 const config = {
